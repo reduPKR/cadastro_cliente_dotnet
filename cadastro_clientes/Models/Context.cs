135a337afee27b93d04f50c4d838593ef9fa93f6
redu <reduardo19942@gmail.com>
2020-12-28 11:08:39 -0300

    Context criado

---
+++ cadastro_clientes/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cadastro_cliente.Models
+{
+    public class Context: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseMySQL("server=localhost;uid=root; pwd=admin;persistsecurityinfo=True;database=cadastro");
+        }
+    }
+}
