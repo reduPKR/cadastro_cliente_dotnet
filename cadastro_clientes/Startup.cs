82aabee1f196e0922e07c4ebce94844f74da75f2
redu <reduardo19942@gmail.com>
2020-12-28 11:47:38 -0300

    Views modificadas

--- cadastro_clientes/Startup.cs
+++ cadastro_clientes/Startup.cs
@@ -37,7 +37,7 @@ namespace cadastro_clientes
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Clientes/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -52,7 +52,7 @@ namespace cadastro_clientes
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Clientes}/{action=Index}/{id?}");
             });
         }
     }
